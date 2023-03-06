01ccbb39d6c7380322951b210ec0efaed0039bfd
dasundoloswala <tharindudasun0@gmail.com>
2023-03-06 17:07:20 +0530

    link firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 
 import 'package:flutter/material.dart';
-
 import 'LoginScreen.dart';
-
-
-
 Future<void> main() async {
   // WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
