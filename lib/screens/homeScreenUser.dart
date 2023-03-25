3c8564eab74f37cee10d9904f04517a2cc1a5df4
OshanBathiya <bathiya.oshan@gmail.com>
2023-03-25 05:58:47 +0530

    Home screen

--- lib/screens/homeScreenUser.dart
+++ lib/screens/homeScreenUser.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:kathaappctse/screens/therapy/client/viewAllTutorialClient.dart';
 import 'package:kathaappctse/screens/voices/client/viewAllVoicesClients.dart';
 
+import 'therapyNotes/ViewAllNote.dart';
+import 'therapyNotes/addNote.dart';
+import 'therapyNotes/viewAddedNotesAll.dart';
+import '../../../utils/config.dart';
+
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -16,22 +22,163 @@ class _HomeScreenState extends State<HomeScreen> {
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
-        body: SafeArea(
-          child: Center(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(Config.app_background2), fit: BoxFit.fill),
+          ),
+          child: SafeArea(
             child: Column(
-              children: <Widget>[
-                ElevatedButton(onPressed:(){
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => AllVoiceClientScreen() ) ) ;},
+              children: [
+                Container(
+                  width: 300.0,
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  decoration: BoxDecoration(
+                    color: Color(0x80FFFFFF),
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(16.0),
+                    ),
+                  ),
                   child: Text(
-                  "Voices",
-                ),),
-                ElevatedButton(onPressed:(){
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => ViewAllTutorialClientClass() ) ) ;},
-                  child: Text(
-                      "Therapy Page client"
-                  ),),
+                    'ආයුබෝවන්!',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 2, // Number of columns in the grid
+                    mainAxisSpacing: 16.0, // Vertical spacing between items
+                    crossAxisSpacing: 16.0, // Horizontal spacing between items
+                    padding: EdgeInsets.all(16.0), // Padding around the grid
+                    children: <Widget>[
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => AllVoiceClientScreen(),
+                          ));
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.purple.shade400,
+                          padding: EdgeInsets.all(16.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.record_voice_over, size: 60.0, color: Colors.white),
+                            SizedBox(height: 16.0),
+                            Text(
+                              // 'Voices',
+                              'හඬ',
+                              style: TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => ViewAllTutorialClientClass(),
+                          ));
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.teal.shade400,
+                          padding: EdgeInsets.all(16.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.healing, size: 60.0, color: Colors.white),
+                            SizedBox(height: 16.0),
+                            Text(
+                              'Therapy Page\nfor Client',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => AllVoiceTherapyNoteScreen(),
+                          ));
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.pink.shade400,
+                          padding: EdgeInsets.all(16.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.notes, size: 60.0, color: Colors.white),
+                            SizedBox(height: 16.0),
+                            Text(
+                              'Therapy note page',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => AllNotesAddedScreen(),
+                          ));
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.blue.shade400,
+                          padding: EdgeInsets.all(16.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.note_add, size: 60.0, color: Colors.white),
+                            SizedBox(height: 16.0),
+                            Text(
+                              'Notes note page',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
